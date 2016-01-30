305401ab0b5631f903b86bff92e3d15f6010960b
Hanna Novikava <arnikasky@gmail.com>
2016-01-31 01:12:06 +0300

    Create pop.js

---
+++ Project-M-end-M/src/js/pop.js
@@ -0,0 +1,14 @@
+<script>
+ $(document).ready(function(){
+//Скрыть PopUp при загрузке страницы
+PopUpHide();
+});
+//Функция отображения PopUp
+function PopUpShow(){
+$("#popup1").show();
+}
+//Функция скрытия PopUp
+function PopUpHide(){
+$("#popup1").hide();
+}
+</script>
