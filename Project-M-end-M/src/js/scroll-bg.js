bdc3ad8733b52d11f4b6d0e772c61f3788e6fa8c
zloykrivedka <zloykrivedka@gmail.com>
2016-01-30 23:10:34 +0300

    Modernized: Scrolling main page

--- Project-M-end-M/src/js/scroll-bg.js
+++ Project-M-end-M/src/js/scroll-bg.js
@@ -1,34 +1,66 @@
-
-
 //Скрипт для эфекта увеличения при скроле
 //И эфекта длюра
-//$('.arkaplan').css('-webkit-filter', 'blur(5px)');
+
+var lastScrollTop = true;
+var scrolling = true;
+
 $(window).scroll(function(e) {
-    var scrollSeviye = $(this).scrollTop();
 
-    if(scrollSeviye>3600){
-        scrollSeviye=3600;
+    console.log($(window).scrollTop()+$(window).height(), $(document).height());
+
+    //Переделать через анимации в SCCках
+    $('.bg-container').css('-webkit-filter', 'blur('+$(window).scrollTop()/500+'px)');
+    $('.bg-container').css('-moz-filter', 'blur('+$(window).scrollTop()/500+'px)');
+    $('.bg-container').css('-o-filter', 'blur('+$(window).scrollTop()/500+'px)');
+    $('.bg-container').css('-ms-filter', 'blur('+$(window).scrollTop()/500+'px)');
+    $('.bg-container').css('filter', 'blur('+$(window).scrollTop()/500+'px)');
+
+    if(($(window).scrollTop()+$(window).height()) == $(document).height()){
+        lastScrollTop = false;
+        scrolling = true;}
+    if($(window).scrollTop() == 0){
+        lastScrollTop = true;
+        scrolling = true;}
+
+    if($(window).scrollTop() > 0 && ($(window).scrollTop()+$(window).height()) < $(document).height() && scrolling == true){
+        if(lastScrollTop == true) {
+            goEnd();
+        } else {
+            goStart()
+        }
+        scrolling = false;
     }
+});
 
-    var buyutme = (scrollSeviye/10000)+1;
-
-    $('.bg-container').css('-webkit-filter', 'blur('+scrollSeviye/1200+'px)');
+$(document).ready(function(e){
+    var pageHeight = $(window).height()*2.5;
+    //console.log(winCentral);
+    $('.content-container').css('height', pageHeight+'px');
+    $('.icon-container').css('height', $(window).height()+'px');
+    $('.icon-container-end-page').css('height', $(window).height()+'px');
 });
 
 //Эфент нажатия на иконку (первую)
 $(function() {
     $('#iconStart').click(function (e) {
-        $.scrollTo('#endPage', 4000);
-        $('.bg-container').removeClass('animate-end');
-        $('.bg-container').addClass('animate-start');
+        scrolling = false;
+       goEnd();
     });
 });
 
 //А вот и в обратную сторону
 $(function() {
-    $('#iconEnd').click(function (e) {
-        $.scrollTo('#startPage', 4000);
-        $('.bg-container').removeClass('animate-start');
-        $('.bg-container').addClass('animate-end');
-    });
+    $('#iconEnd').click(function (e) { scrolling = false; goStart(); });
 });
+
+function goEnd(){
+    $('.bg-container').removeClass('animate-end');
+    $('.bg-container').addClass('animate-start');
+    $.scrollTo($(document).height()-$(window).height(), 2000);
+}
+
+function goStart(){
+    $('.bg-container').removeClass('animate-start');
+    $('.bg-container').addClass('animate-end');
+    $.scrollTo('#startPage', 700);
+}
